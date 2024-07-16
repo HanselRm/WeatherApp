ee5776cff05d41f455fbb38c2cf08d9cb7f9f0cf
HanselRm <20220897@itla.edu.do>
2024-07-16 10:15:04 -0400

    Wather_model

---
+++ WeatherApp/Models/Wather.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherApp.Models
+{
+    public class Wather
+    {
+        public int Id { get; set; }
+        //clima
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+}
--- WeatherApp/Models/cord.cs
+++ WeatherApp/Models/cord.cs
@@ -2,7 +2,9 @@
 {
     public class Cord
     {
+        //longitud
         public double lon { get; set; }
+        //latitud
         public double lat { get; set; }
     }
 }
