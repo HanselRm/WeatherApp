59c5dc90e4ea4e38dec2695764b20eafb8970c84
HanselRm <20220897@itla.edu.do>
2024-07-19 21:18:19 -0400

    Controller

--- WeatherApp/Controllers/HomeController.cs
+++ WeatherApp/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WeatherApp.Models;
+using WeatherApp.Services;
 
 namespace WeatherApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly WeatherServices _weatherServices;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, WeatherServices weatherServices)
         {
             _logger = logger;
+            _weatherServices = weatherServices;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(string city)
         {
-            return View();
+            string apiKey = "aab013cd93f51a9e440646ffa4d9cbf4";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}";
+            return View(await _weatherServices.getWeather(url));
         }
 
         public IActionResult Privacy()
