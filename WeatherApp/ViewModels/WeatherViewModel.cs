2d7f3d68ddf28af4d4fcf84f302559d96eedf7ad
HanselRm <20220897@itla.edu.do>
2024-07-19 20:15:51 -0400

    ViewModel

---
+++ WeatherApp/ViewModels/WeatherViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherApp.ViewModels
+{
+    public class WeatherViewModel
+    {
+        public string city { get; set; }
+        public string weather { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+}
