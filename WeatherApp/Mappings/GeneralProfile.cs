ba621e3ec0088d47af7ffa58a4ff919312bc977a
HanselRm <20220897@itla.edu.do>
2024-07-19 21:09:20 -0400

    Automapper

---
+++ WeatherApp/Mappings/GeneralProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using WeatherApp.Models;
+using WeatherApp.ViewModels;
+
+namespace WeatherApp.Mappings
+{
+    public class GeneralProfile : Profile
+    {
+        public GeneralProfile()
+        {
+            CreateMap<WeatherViewModel, Wather>();
+        }
+    }
+}
