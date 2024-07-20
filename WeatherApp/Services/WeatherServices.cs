3d3742bdd49b3313ae788fb6bfc3df21992b9cd3
HanselRm <20220897@itla.edu.do>
2024-07-19 21:06:48 -0400

    Services

---
+++ WeatherApp/Services/WeatherServices.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Newtonsoft.Json;
+using WeatherApp.Models;
+using WeatherApp.ViewModels;
+
+namespace WeatherApp.Services
+{
+    public class WeatherServices
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IMapper _mapper;
+
+        public WeatherServices(HttpClient httpClient, IMapper mapper)
+        {
+            _httpClient = httpClient;
+            _mapper = mapper;
+        }
+        public async Task<WeatherViewModel> getWeather(string url, string city, string apiKey)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+            if(response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync();
+                Wather weather = JsonConvert.DeserializeObject<Wather>(json);
+
+                return _mapper.Map<WeatherViewModel>(weather);
+            }
+            else
+            {
+                throw new Exception($"Error: {response.StatusCode}");
+            }
+            
+        }
+    }
+}
