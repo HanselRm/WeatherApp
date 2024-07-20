a1503be2572065a32ea4753f5e533568ba34cbd2
HanselRm <20220897@itla.edu.do>
2024-07-19 21:10:36 -0400

    change name

--- WeatherApp/ViewModels/WeatherViewModel.cs
+++ WeatherApp/ViewModels/WeatherViewModel.cs
@@ -3,7 +3,7 @@
     public class WeatherViewModel
     {
         public string city { get; set; }
-        public string weather { get; set; }
+        public string main { get; set; }
         public string description { get; set; }
         public string icon { get; set; }
     }
